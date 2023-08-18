be0beb79491112f6e56f375402b50d9c41663bc5
ithiel883 <ajuka.zephiniah@gmail.com>
2023-08-18 15:12:51 +0100

    done with image generation

--- app.js
+++
@@ -1,8 +0,0 @@
-let img = document.getElementById('#hello')
-
-console.log(img)
-
-document.addEventListener('htmx:afterRequest', function(evt) {
-
-    let url = JSON.parse(evt.detail.xhr.response).urls.regular
-});
